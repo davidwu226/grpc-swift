839a869e5eeff8cd3d2582e62c49c98d88af0c85
Dave <davidwu22@hotmail.com>
2017-11-06 07:34:30 -0800

    Initial check-in.

---
+++ Sources/SwiftGRPC.h
@@ -0,0 +1,18 @@
+//
+//  SwiftGRPC.h
+//  SwiftGRPC
+//
+//  Created by David Wu on 11/6/17.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftGRPC.
+FOUNDATION_EXPORT double SwiftGRPC_VersionNumber;
+
+//! Project version string for SwiftGRPC.
+FOUNDATION_EXPORT const unsigned char SwiftGRPC_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftGRPC/PublicHeader.h>
+
+#import <CgRPC.h>
